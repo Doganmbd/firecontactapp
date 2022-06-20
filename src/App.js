ef7f848de58a535dae37aaa0e9dbb95bcebbf2f2
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-06-20 07:25:38 +0300

    state,initialvalues,formcomponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import {useState} from 'react' ;
 import FormComponent from './components/form/FormComponent';
 import Contacts from './components/contacts/Contacts';
 
+/* her sayfayı yenilediğimde forma yazılanları boşaltmak istiyorum bu yüzden boş stringe eşitliyorum ve state initialvalues ekliyorum*/
+
+const initialValues={
+  username : "" ,
+  phoneNumber : "" ,
+  gender : "NO INFO"
+}
+
+
 function App() {
+  /* Forma yazılacak bilgileri bir değişkende tutmakiçin bir state oluşturuyorum.Ve forma göndermek için buradan info ve setinfo değerlerini FormComponent e ekliyorum */
+  const [info, setInfo] = useState(initialValues)
+
+
   return (
     <div className="App">
       <Contacts />
-      <FormComponent />
+      <FormComponent info={info} setInfo={setInfo} />
     </div>
   );
 }
