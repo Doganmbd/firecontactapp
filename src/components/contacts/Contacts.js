174c70dc96277cd4306c0a4de4168472f7d77242
Mustafa Bilal Dogan <mbilaldogan90@gmail.com>
2022-06-20 07:09:30 +0300

    contacts create

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+    Table,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableBody,
+    Paper
+} from "@mui/material";
+
 
 const Contacts = () => {
-  return (
-    <div>Contacts</div>
-  )
-}
 
-export default Contacts
+  return (
+    <div>
+      <h2 className="contact-header">Contacts</h2>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Username</TableCell>
+              <TableCell align="right">Phone Number</TableCell>
+              <TableCell align="right">Gender</TableCell>
+              <TableCell align="right">Delete</TableCell>
+              <TableCell align="right">Edit</TableCell>
+            </TableRow>
+          </TableHead>        
+         
+          <TableBody>
+            <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>            
+              <TableCell colSpan={5} align="center">Loading</TableCell>             
+            </TableRow>
+
+            <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>            
+            <TableCell colSpan={5} align="center">Ekrana yazacak veri bulunamadı</TableCell>             
+          </TableRow>
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+export default Contacts;
